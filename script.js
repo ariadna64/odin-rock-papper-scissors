5ccce39defda6fb350f0479d55e2099b757a8a5b
ariadna64 <157072066+ariadna64@users.noreply.github.com>
2024-03-23 09:33:04 -0300

    Agregue while loop al final. Si score llega a 5, termina el programa.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let playerScore = 0
-let computerScore = 0
+let computerScore = 0 // SCORES DEFINIDOS EN 0 PARA TOMARLOS LUEGO EN LA FUNCION
 
-function getComputerChoice() {
+function getComputerChoice() { // FUNCION QUE DEVUELVE PIEDRA, PAPEL O TIJERA RANDOM
     const elements = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * elements.length);
     
@@ -16,15 +16,13 @@ function playRound(player, computer) {
     let round_lost = "Oh no! You chose "+player+" and the computer chose "+computer+". You lost the round! ";
     let round_won = "Yes! You chose "+player+" and the computer chose "+computer+". You won the round! ";
 
-    console.log(computer)
-
     
     // IF PLAYER WINS
     if ((player === "rock" &&  computer === "scissors") ||
     (player === "paper" && computer === "rock") ||
     (player === "scissors" && computer === "paper")) {
-        playerScore += 1;
-        result = round_won + "You\'r score is " + playerScore+". And the computer score is "+computerScore;
+        playerScore += 1; // Si ganas aumenta el score
+        result = round_won + "Your score is " + playerScore+". And the computer score is "+computerScore;
         console.log(result);
         
         if (playerScore === 5) {
@@ -35,8 +33,8 @@ function playRound(player, computer) {
     else if ((player === "scissors" &&  computer === "rock") ||
     (player === "rock" && computer === "paper") ||
     (player === "paper" && computer === "scissors")){
-        computerScore += 1
-        result = round_lost + "You\'r score is " + playerScore+". And the computer score is "+computerScore;
+        computerScore += 1 // Si perdes aumenta el score de la computadora
+        result = round_lost + "Your score is " + playerScore+". And the computer score is "+computerScore;
         console.log(result);
     }
         if (computerScore === 5) {
@@ -48,11 +46,12 @@ function playRound(player, computer) {
             console.log(result);
         
         }
-    
     }
 
+// WHILE LOOP QUE SE EJECUTA MIENTRAS LOS SCORES SEAN MENORES A 5. TIENE EL PROMPT Y EL ELEMENTO RANDOM DE LA PRIMERA FUNCION.
 
-const playerSelection = prompt("Rock, Paper or Scissors? ").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
+while (playerScore < 5 && computerScore < 5) { 
+    const playerSelection = prompt("Rock, Paper or Scissors? ").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+}
