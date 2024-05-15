885fa2b936104a7f42979415922de120c969cad0
ariadna64 <157072066+ariadna64@users.noreply.github.com>
2024-05-15 17:51:51 -0300

    Finished the ui. Added a button to start the game again.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let playerScore = 0;
 let computerScore = 0; // SCORES DEFINIDOS EN 0 PARA TOMARLOS LUEGO EN LA FUNCION
 
-const goalScore = 5;
+const goalScore = 5; //SCORE OBJETIVO
 
 function getComputerChoice() { // FUNCION QUE DEVUELVE PIEDRA, PAPEL O TIJERA RANDOM
-    const elements = ["rock", "paper", "scissors"];
+    const elements = ["ROCK", "PAPER", "SCISSORS"];
     const random = Math.floor(Math.random() * elements.length);
     
     return elements[random];
-    
+
 }
 
 //BOTONES
@@ -18,7 +18,7 @@ rock.textContent = "ROCK";
 document.body.appendChild(rock);
 
 eventoRock = rock.addEventListener("click",() => {
-    console.log(playRound("rock", getComputerChoice()));
+    console.log(playRound("ROCK", getComputerChoice()));
 })
 
 
@@ -27,7 +27,7 @@ paper.textContent = "PAPER";
 document.body.appendChild(paper);
 
 eventoPaper = paper.addEventListener("click",() => {
-    console.log(playRound("paper", getComputerChoice()));
+    console.log(playRound("PAPER", getComputerChoice()));
 })
 
 
@@ -36,8 +36,37 @@ scissors.textContent = "SCISSORS";
 document.body.appendChild(scissors);
 
 eventoScissors = scissors.addEventListener("click",() => {
-    console.log(playRound("scissors", getComputerChoice()));
+    console.log(playRound("SCISSORS", getComputerChoice()));
 })
+
+// DIV QUE MUESTRA LOS RESULTADOS
+
+let div = document.createElement("div");
+document.body.appendChild(div);
+
+//DIVS DEL SCORE
+
+let divPcScore = document.createElement("div");
+document.body.appendChild(divPcScore);
+
+
+let divPlayerScore = document.createElement("div");
+document.body.appendChild(divPlayerScore);
+
+//BOTON PARA RECARGAR LA PAGINA
+
+let buttonStart = document.createElement("button");
+buttonStart.textContent = "START AGAIN"
+
+document.body.appendChild(buttonStart);
+    
+function reloadClick() {
+    window.location.reload(); //FUNCION QUE RECARGA LA PAGINA
+}
+    
+buttonStart.addEventListener("click", reloadClick);
+
+
 
 
 function playRound(player, computer) {
@@ -45,29 +74,33 @@ function playRound(player, computer) {
     let result = "";
     let round_lost = "Oh no! You chose "+player+" and the computer chose "+computer+". You lost the round! ";
     let round_won = "Yes! You chose "+player+" and the computer chose "+computer+". You won the round! ";
-    
-    if ((player !== "rock") || (player !== "paper") || (player !== "scissors")) {  // Si el input del player es distinto
+
+ while ((playerScore !== goalScore) && (computerScore !== goalScore)) {  
+
+    if ((player !== "ROCK") || (player !== "PAPER") || (player !== "SCISSORS")) {  // Si el input del player es distinto
         result = div.textContent = "Invalid input. Try again.";                                       // a rock, paper o scissors. 
 
     } 
 
     // IF PLAYER WINS
-    if ((player === "rock" &&  computer === "scissors") ||
-    (player === "paper" && computer === "rock") ||
-    (player === "scissors" && computer === "paper")) {
+    if ((player === "ROCK" &&  computer === "SCISSORS") ||
+    (player === "PAPER" && computer === "ROCK") ||
+    (player === "SCISSORS" && computer === "PAPER")) {
         playerScore += 1; // Si ganas aumenta el score
-        result = round_won + "Your score is " + playerScore+". And the computer score is "+computerScore+".";
+        result = round_won;
+        divPlayerScore.textContent = "Your score: "+playerScore+".";
         
         if (playerScore === goalScore) {
             return div.textContent = "You won the match!"
         }
     
     }  // IF COMPUTER WINS
-    else if ((player === "scissors" &&  computer === "rock") ||
-    (player === "rock" && computer === "paper") ||
-    (player === "paper" && computer === "scissors")){
+    else if ((player === "SCISSORS" &&  computer === "ROCK") ||
+    (player === "ROCK" && computer === "PAPER") ||
+    (player === "PAPER" && computer === "SCISSORS")){
         computerScore += 1 // Si perdes aumenta el score de la computadora
-        result = round_lost + "Your score is " + playerScore+". And the computer score is "+computerScore+".";
+        result = round_lost;
+        divPcScore.textContent = "Computer score: "+computerScore+".";
     }
         if (computerScore === goalScore) {
             return div.textContent = "You lost the match!"
@@ -79,23 +112,13 @@ function playRound(player, computer) {
         }
    
     return div.textContent = result
+    
+
     }
 
+    
 
+    }
 
-
-// WHILE LOOP QUE SE EJECUTA MIENTRAS LOS SCORES SEAN MENORES A 5. TIENE EL PROMPT Y EL ELEMENTO RANDOM DE LA PRIMERA FUNCION.
-
-let div = document.createElement("div");
-document.body.appendChild(div);
-
-
-if ((playerScore < goalScore) && (computerScore < goalScore)) {
-   div.textContent = play
-} else if (playerScore === goalScore) {
-    div.textContent = won;
-} else {
-   div.textContent = lost;
-}
 
 
